e606ac677411af0ee39eaf1eed05be4911754de0
ashudvlps <ashudvlps@gmail.com>
2017-04-02 17:50:17 -0700

    V0.01a

--- day_three.py
+++ day_three.py
@@ -1,1 +1,11 @@
-#just some comments
+#following line is to get the input
+import sys
+N = int(input().strip()) #since this is supposed to be number, I need to change it to integer
+if N%2 == 1:
+    print("Weird")
+elif 2<= N <=5:
+    print("Not Weird")
+elif 6<= N <=20:
+    print("Weird")
+else:
+    print("Not Weird")
